6c23b3dcf5adf438ed8c64348b9082d952cc9eda
rfsyhb <rafisyihab3@gmail.com>
2024-04-01 04:55:19 +0700

    Add language and dark mode functionality, and create LocaleContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { getUserLogged, putAccessToken } from "./utils/network-data";
+import LocaleContext from "./contexts/LocaleContext";
 
 function App() {
   const [authedUser, setAuthedUser] = React.useState(null);
   const [initializing, setInitializing] = React.useState(true);
+  const [language, setLanguage] = React.useState("id");
+  const [darkMode, setDarkMode] = React.useState(false);
+
+  // setLanguage
+  const toggleLanguage = () => {
+    setLanguage((prevLanguage) => {
+      return prevLanguage === "id" ? "en" : "id";
+    });
+  };
+
+  // setDarkMode
+  const toggleDark = () => {
+    setDarkMode((prevDarkMode) => {
+      return prevDarkMode === false ? true : false;
+    });
+  };
+
+  const contextValues = React.useMemo(() => {
+    return {
+      language,
+      toggleLanguage,
+      darkMode,
+      toggleDark,
+    };
+  }, [language, darkMode]);
 
   // login sukses
   const onLoginHandler = async ({ accessToken }) => {
@@ -41,34 +67,38 @@ function App() {
   // belum authed
   if (authedUser === null) {
     return (
-      <div className="app-container">
-        <header></header>
-        <main>
-          <Routes>
-            <Route path="/*" element={<p>login</p>} />
-            <Route path="/register" element={<p>register</p>} />
-          </Routes>
-        </main>
-      </div>
+      <LocaleContext.Provider value={contextValues}>
+        <div className="app-container">
+          <header></header>
+          <main>
+            <Routes>
+              <Route path="/*" element={<p>login</p>} />
+              <Route path="/register" element={<p>register</p>} />
+            </Routes>
+          </main>
+        </div>
+      </LocaleContext.Provider>
     );
   }
 
   // user authed
   return (
-    <div className="app-container">
-      <header>
-        <h1>Notes App</h1>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<p>homepage</p>} />
-          <Route path="/archived" element={<p>archivedpage</p>} />
-          <Route path="/note/:id" element={<p>detailNote</p>} />
-          <Route path="/note/add" element={<p>addpage</p>} />
-          <Route path="*" element={<p>notfound</p>} />
-        </Routes>
-      </main>
-    </div>
+    <LocaleContext.Provider value={contextValues}>
+      <div className="app-container">
+        <header>
+          <h1>Notes App</h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<p>homepage</p>} />
+            <Route path="/archived" element={<p>archivedpage</p>} />
+            <Route path="/note/:id" element={<p>detailNote</p>} />
+            <Route path="/note/add" element={<p>addpage</p>} />
+            <Route path="*" element={<p>notfound</p>} />
+          </Routes>
+        </main>
+      </div>
+    </LocaleContext.Provider>
   );
 }
 
