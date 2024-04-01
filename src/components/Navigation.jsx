de9d45d4f029e09d065d42fff7cb026dd2005cd6
rfsyhb <rafisyihab3@gmail.com>
2024-04-02 00:27:45 +0700

    Refactor Navigation component to improve code structure and UI

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,33 +2,32 @@ import React from "react";
 import { Link } from "react-router-dom";
 import LocaleContext from "../contexts/LocaleContext";
 import PropTypes from "prop-types";
+import { FiMoon, FiSun } from "react-icons/fi";
 
 function Navigation({ logout, name }) {
   const { locale, toggleLocale, theme, toggleTheme } =
     React.useContext(LocaleContext);
   return (
-    <nav className="navigation">
-      <ul>
-        <li>
-          <button className="toggle-locale" onClick={toggleLocale}>
-            {locale === "id" ? "en" : "id"}
-          </button>
-        </li>
-        <li>
-          <button className="toggle-theme" onClick={toggleTheme}>
-            {theme === "light" ? "dark" : "light"}
-          </button>
-        </li>
-        <li>
-          <Link to="/archived">{locale === "id" ? "Catatan terarsip" : "Archived Note"}</Link>
-        </li>
-        <li>
-          <button className="button-logout" onClick={logout}>
-            {name}
-          </button>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav className="navigation">
+        <ul>
+          <li>
+            <Link to="/archived">
+              {locale === "id" ? "Catatan terarsip" : "Archived Note"}
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <button className="toggle-locale" onClick={toggleLocale}>
+        {locale === "id" ? "en" : "id"}
+      </button>
+      <button className="toggle-theme" onClick={toggleTheme}>
+        {theme === "light" ? <FiMoon /> : <FiSun />}
+      </button>
+      <button className="button-logout" onClick={logout}>
+        {name}
+      </button>
+    </>
   );
 }
 
