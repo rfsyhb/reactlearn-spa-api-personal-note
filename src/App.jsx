648e403bf93c3a6c400f50384a43c373b0fffd1d
rfsyhb <rafisyihab3@gmail.com>
2024-04-06 04:43:58 +0700

    useMemo, onLoginSuccessHandler, onLogoutHandler, useEffect fetch data user, useEffect set theme, app container

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,98 @@
 import React from "react";
 import useLocale from "./hooks/useLocale";
 import useTheme from "./hooks/useTheme";
+import * as NetworkData from "./api/network-data";
+import AppContext from "./contexts/AppContext";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  // inti App
+  // App state
   const [authedUser, setAuthedUser] = React.useState(null);
   const [initializing, setInitialziing] = React.useState(true);
 
-  // useContext: language / theme
+  // useContext: custom hooks for language / theme
   const [locale, toggleLocale] = useLocale();
   const [theme, toggleTheme] = useTheme();
+
+  // useContext: useMemo
+  const contextValues = React.useMemo(() => {
+    return { locale, toggleLocale, theme, toggleTheme };
+  }, [locale, theme, toggleLocale, toggleTheme]);
+
+  // Login Success
+  async function onLoginSuccessHandler({ accessToken }) {
+    NetworkData.putAccessToken(accessToken);
+    const { data } = await NetworkData.getUserLogged();
+
+    setAuthedUser(data);
+  }
+
+  // User Logout
+  function onLogoutHandler() {
+    setAuthedUser(null);
+    NetworkData.putAccessToken("");
+  }
+
+  // Fetch data user yang login
+  React.useEffect(() => {
+    const fetchUser = async () => {
+      const { data } = await NetworkData.getUserLogged();
+      setAuthedUser(data);
+      setInitialziing(false);
+    };
+
+    fetchUser();
+  }, []);
+
+  // Merubah theme setiap state berubah
+  React.useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  // Saat loading
+  if (initializing) {
+    // return null;
+    return <p>initializing data</p>;
+  }
+
+  // Jika user belum authenticate
+  if (authedUser === null) {
+    return (
+      <AppContext.Provider value={contextValues}>
+        <div className="app-container">
+          <header></header>
+          <main>
+            <Routes>
+              <Route path="/*" element={<p>Login</p>} />
+              <Route path="/register" element={<p>Register</p>} />
+            </Routes>
+          </main>
+        </div>
+      </AppContext.Provider>
+    );
+  }
+
+  // User authenticated
+  return (
+    <AppContext.Provider value={contextValues}>
+      <div className="app-container">
+        <header>
+          <h1>
+            <Link to="/">{locale === "id" ? "Aplikasi Catatan" : "Notes App"}</Link>
+          </h1>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<p>HomePage</p>} />
+            <Route path="/archived" element={<p>Register</p>} />
+            <Route path="/notes/:id" element={<p>Register</p>} />
+            <Route path="/note/add" element={<p>Register</p>} />
+            <Route path="/*" element={<p>notfound</p>} />
+          </Routes>
+        </main>
+      </div>
+    </AppContext.Provider>
+  );
 }
 
 export default App;
